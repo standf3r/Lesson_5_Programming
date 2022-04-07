dbe189cc096d04935520df1c0d4e56b0bd2fac98
standfer <vertigl@yandex.ru>
2022-04-07 21:21:08 +0800

    Задача 34

---
+++ Homework_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34. Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных чисел в массиве.
+
+const int size=12;
+
+int[] array = new int[size];
+Console.Write("[");
+for(int i = 0; i < size; i++)
+{
+  
+  array[i] = new Random().Next(100,1000);
+  Console.Write(array[i] + " ");
+}
+Console.Write("]");
+Console.WriteLine();
+
+int plus = 0;
+int minus = 0;
+for(int i = 0; i < array.Length; i++){
+  if (array[i] % 2 == 0){
+    plus += 1;
+  }else{
+    minus += 1;
+  }
+}
+
+Console.Write($"Всего четных = {plus}");
