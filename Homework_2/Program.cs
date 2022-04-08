a8971c4c554cc5154325b1e40f42e40567f7a048
standfer <vertigl@yandex.ru>
2022-04-08 19:30:09 +0800

    ДЗ. Задача 36

---
+++ Homework_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//36. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int size = 10;
+int[] array = new int[size];
+int a = 0;
+for(int i = 0; i < size; i++){
+array[i] = new Random().Next(1,10);
+Console.Write(array[i] + " ");
+}
+for(int i=0; i < size; i++)
+{
+    if(i % 2 == 0)
+    {
+        a = a+array[i];
+    }
+}
+Console.WriteLine($"сумма чисел: {a}");
+
